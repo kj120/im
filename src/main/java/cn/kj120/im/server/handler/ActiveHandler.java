226747c9df335b013971bf9652e82babc3cfd2d9
pcg <765018483@qq.com>
2019-12-27 23:04:35 +0800

    更新过

--- src/main/java/cn/kj120/im/server/handler/ActiveHandler.java
+++ src/main/java/cn/kj120/im/server/handler/ActiveHandler.java
@@ -1,34 +1,13 @@
 package cn.kj120.im.server.handler;
 
-import cn.kj120.im.common.CacheManage;
-import cn.kj120.im.server.store.ChannelHandlerContextCache;
-import cn.kj120.im.server.config.Session;
-import cn.kj120.im.server.config.ChannelAttr;
-import cn.kj120.im.server.util.SessionUtil;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.netty.util.Attribute;
-
-import java.util.Date;
 
 
 public class ActiveHandler extends ChannelInboundHandlerAdapter {
-
-    private CacheManage<String, ChannelHandlerContext> cacheManage;
-
-    {
-        cacheManage = ChannelHandlerContextCache.singleten();
-    }
-
+    
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        String sessionId = SessionUtil.createSessionId(ctx.channel());
-
-        Attribute<Session> attr = ctx.channel().attr(ChannelAttr.SESSION);
-        Session session = new Session(sessionId, new Date());
-        attr.set(session);
-
-        cacheManage.put(sessionId, ctx);
         super.channelActive(ctx);
     }
 }
