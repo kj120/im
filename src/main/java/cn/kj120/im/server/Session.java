d8a25c029058fef966c00555290c8b3dc428e229
pcg <daemon.pcg@gmail.com>
2019-12-27 15:59:41 +0800

    更新readme

--- src/main/java/cn/kj120/im/server/Session.java
+++ src/main/java/cn/kj120/im/server/Session.java
@@ -5,11 +5,22 @@ import lombok.Data;
 
 import java.util.Date;
 
+/**
+ * 会话信息
+ * @param <T> 其他业务数据
+ */
 @Data
 @AllArgsConstructor
-public class Session {
+public class Session<T> {
 
     private String sessionId;
 
     private Date connectDate;
+
+    private T data;
+
+    public Session(String sessionId, Date connectDate) {
+        this.sessionId = sessionId;
+        this.connectDate = connectDate;
+    }
 }
